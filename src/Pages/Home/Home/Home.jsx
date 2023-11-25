cdca13aa5e3960e848e76f715fefc57e5575ee43
tazbir01 <tajberahamad@gmail.com>
2023-11-25 12:55:03 +0600

    added testimonial section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../Banner/Banner";
 import OurServices from "../OurServices/OurServices";
+import Testimonial from "../Testimonial/Testimonial";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <div className="max-w-6xl mx-auto space-y-10 my-10">
-            <OurServices></OurServices>
+                <OurServices></OurServices>
+                <Testimonial></Testimonial>
             </div>
         </div>
     );
---
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -0,0 +1,46 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+import { useEffect, useState } from 'react';
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+
+const Testimonial = () => {
+    const [reviews, setReviews] = useState([])
+
+    useEffect(() => {
+        fetch('./review.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+    return (
+        <div>
+            <div className="divider text-2xl font-bold">Testimonial</div> 
+            <>
+                <Swiper
+                    cssMode={true}
+                    navigation={true}
+                    pagination={false}
+                    mousewheel={true}
+                    keyboard={true}
+                    modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                    className="mySwiper"
+                >
+                    {
+                        reviews.map(review => <SwiperSlide key={review.id}>
+                            <div className='space-y-3 px-16'>
+                                <p className='text-2xl'><FaQuoteLeft></FaQuoteLeft></p>
+                                <p className='text-xl'>{review.quote}</p>
+                                <div className='flex justify-end'><p className='text-2xl'><FaQuoteRight></FaQuoteRight></p></div>
+                                <p className='text-2xl font-bold border-t-2'>{review.author}</p>
+                            </div>
+                        </SwiperSlide>)
+                    }
+                </Swiper>
+            </>
+        </div>
+    );
+};
+
+export default Testimonial;
