f351a9ec97c0ed37c5c57299028644d03c602414
tazbir01 <tajberahamad@gmail.com>
2023-11-26 12:58:45 +0600

    add logo

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
+import logo from "../../../../src/assets/home/logo.png"
 
 const Navbar = () => {
-    const {user, logout} = useAuth()
+    const { user, logout } = useAuth()
 
-    const handleLogout = ()=>{
+    const handleLogout = () => {
         logout()
     }
     const links = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/register'>Register</Link></li>
-        
+
         {
             user ? <button onClick={handleLogout}>Logout</button>
-            :<li><Link to='/login'>Login</Link></li>
+                : <li><Link to='/login'>Login</Link></li>
         }
     </>
     return (
@@ -24,14 +25,17 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                       {links}
+                        {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">WorkWave</a>
+                <div className="flex items-center">
+                    <img className="w-14" src={logo} alt="" />
+                    <a className="font-semibold text-xl">WorkWave</a>
+                </div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                   {links}
+                    {links}
                 </ul>
             </div>
             <div className="navbar-end">
