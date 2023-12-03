ac0f8ab720515b9ec418b5b1ac6e145bc0e82441
tazbir01 <tajberahamad@gmail.com>
2023-12-03 14:33:47 +0600

    added admin table

--- src/Pages/Dashboard/AllEmployeeList/AllEmployeeList.jsx
+++ src/Pages/Dashboard/AllEmployeeList/AllEmployeeList.jsx
@@ -1,8 +1,37 @@
+import useUsers from "../../../hooks/useUsers";
 
 const AllEmployeeList = () => {
+    const {users} = useUsers()
+    console.log(users)
+
+
     return (
         <div>
-            
+            <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Designation</th>
+            <th>Role</th>
+            <th>Verified</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((user, idx) => <tr key={user._id}>
+              <th>{idx + 1}</th>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.designation}</td>
+              <td>{user.role}</td>
+              <td>{user.verify_status}</td>
+            </tr>)
+          }
+        </tbody>
+      </table>
         </div>
     );
 };
